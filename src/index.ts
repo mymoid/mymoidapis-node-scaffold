2053229e52c43436496da3125a01e291b5b7b7b0
Denis Juan Alonso Glez <denjalonso@gmail.com>
2020-02-10 17:13:03 +0000

    Assign log level

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
-import logger, {LogLevelDesc} from 'loglevel'
+import * as logger from 'loglevel'
 import AWSXRay from 'aws-xray-sdk'
 import startServer from './start'
 import http from 'http'
 import https from 'https'
 
-const isTest = process.env.NODE_ENV !== 'test'
-const logLevel: LogLevelDesc = isTest ? 'warn' : 'info'
+const isTest = process.env.NODE_ENV === 'test'
+const isProduction = process.env.NODE_ENV === 'production'
+const logLevel: logger.LogLevelDesc = isTest ? 'info' : 'debug'
 
 logger.setLevel(logLevel)
 
-if (!isTest) {
+if (isProduction) {
   AWSXRay.captureHTTPsGlobal(http, true)
   AWSXRay.captureHTTPsGlobal(https, true)
   AWSXRay.setLogger(logger)
